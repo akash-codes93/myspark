2d3e775f32c632a596c72b9198c6ff1f3323be02
Akash Gupta <akash.gupta@adra.com>
2019-03-25 17:37:57 +0530

    friends by age

--- ratings-counter.py
+++ ratings-counter.py
@@ -1,11 +1,12 @@
-from pyspark import SparkConf, SparkContext
 import collections
+
+from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf=conf)
 
 lines = sc.textFile("data/ml-100k/u.data")
-ratings = lines.map(lambda x:x.split()[2])
+ratings = lines.map(lambda x: x.split()[2])
 
 result = ratings.countByValue()
 sortedResults = collections.OrderedDict(sorted(result.items()))
